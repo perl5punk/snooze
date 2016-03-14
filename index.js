f2633f4fe8a67d1a64bb3ee0651e8428b7326b61
perl5punk <perlpunk@gmail.com>
2016-03-14 16:43:39 -0600

    do some authentication

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ var urlHelper   = require('url');
 var _           = require('underscore');
 var guid        = require('guid');
 var jwt         = require('jsonwebtoken');
+var bbJWT       = require("bbjwt-client");
 
 var AWS         = require('aws-sdk');
 var crypto      = require('crypto');
 
 var logger      = require('./util/logger');
-var bbJWT       = require("bbjwt-client");
+var sdc         = require('./util/metrics');
 
 var tasks       = require('./core/tasks');
 var runner      = require('./core/runner');
-
-var sdc         = require('./util/metrics');
 
 //var SERVERID = uuid.v4();
 
@@ -55,31 +54,56 @@ app.get('/', function (req, res, next) {
 app.post('/add', function (req, res, next) {
 
     var task = req.body.task;
-    console.log('adding a task?',task);
-    // check requirements for adding a thing
-    if (task)
-    {
 
-        tasks.addTask(task,function(err,taskId){
-            if (err)
-            {
-                returnErrorJson(res, { message: err, success: false });
-            }
-            else
-            {
-                //sdc.incrMetric('taskAdded');
-                returnSuccessJson(res, { id: taskId, success: true, message: 'Task added' });
-            }
+    authenticate(req, res, function(jwt){
 
-        });
+        // check requirements for adding a thing
+        if (task)
+        {
 
-    }
-    else
-    {
-        returnError(res, 'no task specified, wtf?!');
-    }
+            tasks.addTask(task,function(err,taskId){
+                if (err)
+                {
+                    logger.logError('Error occurred adding a task',task);
+                    sdc.incrMetric('addTaskError');
+                    returnErrorJson(res, { message: err, success: false });
+                }
+                else
+                {
+                    sdc.incrMetric('addTaskSuccess');
+                    returnSuccessJson(res, { id: taskId, success: true, message: 'Task added' });
+                }
+
+            });
+
+        }
+        else
+        {
+            returnError(res, 'no task specified, wtf?!');
+        }
+
+    });
+
 
 });
+
+function authenticate(req, res, callback)
+{
+    var decodedToken = '';
+    if (process.env.JWT_HEADER)
+    {
+        var token = req.get(process.env.JWT_HEADER);
+        decodedToken = bbjwt.decodeToken(token);
+        var reqIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        if (decodedToken === false)
+        {
+            sdc.incrMetric('userAuthenticationFailed');
+            returnError(res, 'Invalid JWT from '+reqIP);
+            return;
+        }
+    }
+    callback && callback(decodedToken);
+}
 
 function returnError(res,err)
 {
