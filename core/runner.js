cfe6bfcd0ac3c4710f6c3685ba1ebb3943a3e38b
perl5punk <perlpunk@gmail.com>
2016-03-15 17:15:24 -0600

    improved error handling and status update

--- core/runner.js
+++ core/runner.js
@@ -43,57 +43,65 @@ Runner.prototype.startTask = function(task)
 
     if (task)
     {
-        tasks.setStatus(task.id, tasks.RUNNING);
-        var childProcess = fork('./core/runtask');
-        childProcess.send( task );
-        childProcess.on('message', function(msg,task) {
 
-            // this will be logged
-            console.info(msg,task);
+        try
+        {
 
-            if (msg.result)
-            {
-                task.updateTask(task.id, { result: msg.result });
+            tasks.setStatus(task.id, tasks.RUNNING);
+
+            var childProcess = fork('./core/runtask');
+            childProcess.send( task );
+            childProcess.on('message', function(data) {
+
+                // this will be logged
+                console.info('Got a message from the child; '+data);
+                if (data.result)
+                {
+                    console.info(data);
+                    tasks.updateTask(task.id, data,function(err,data){
+                        err && console.log(err);
+                    });
             }
 
-        });
-        childProcess.on('error', function(err) {
+            });
+            childProcess.on('error', function(err) {
 
-            // this will be logged
-            console.error(err);
-            if (err)
-            {
-                task.updateTask(task.id, { error: err });
-            }
+                // this will be logged
+                console.error(err);
+                if (err)
+                {
+                    tasks.updateTask(task.id, { error: err });
+                }
 
-        });
-        childProcess.on('close', function(code) {
+            });
+            childProcess.on('close', function(code) {
 
-            if (code)
-            {
+                if (code)
+                {
 
-                // might need to clean-up tasks if it didn't exit successfully
-                console.log("child runtask exited, NO GOOD "+code);
-                tasks.updateTask(task.id, {status: code, result: code}, function(err,data){
+                    // might need to clean-up tasks if it didn't exit successfully
+                    console.log("child runtask exited, NO GOOD "+code);
+                    tasks.setStatus(task.id, code, function(err,data){
+                        console.log("child runtask exit status set "+err,data);
+                    });
 
-                    console.log("child runtask exit status set "+err,data);
+                }
+                else
+                {
+                    tasks.setStatus(task.id, tasks.SUCCESS, function(err,data){
+                        console.log("child runtask exit status set "+err,data);
+                    });
+                    console.log("child runtask exited, all good");
+                }
 
-                });
+            });
 
-            }
-            else
-            {
-                tasks.updateTask(task.id, {status: code, result: tasks.SUCCESS}, function(err,data){
-
-                    console.log("child runtask exit status set "+err,data);
-
-                });
-                console.log("child runtask exited, all good");
-            }
-
-        });
-
-        console.log("runtask started");
+        }
+        catch (e)
+        {
+            console.error(e);
+            tasks.updateTask(task.id, { error: e });
+        }
     }
     else
     {
