085a5e9044141cd7dad0189fc9aab113d2e08d8d
Bradley Bouley <bradleyjamesbouley@gmail.com>
2016-03-15 17:29:50 -0600

    Updated tests for /add route and /cancel route, passing with config credentials

--- spec/snooze-core.spec.js
+++ spec/snooze-core.spec.js
@@ -6,8 +6,11 @@ var assert              = require('assert'),
     jwt                 = require('jsonwebtoken'),
     AWS                 = require('aws-sdk');
 
-var testEnvVars         = require('../test/test.env.inc.js');
-var index               = require('../index.js');
+var testEnvVars         = require('../test/test.env.js');
+
+var token = jwt.sign({ foo: 'bar', expires: (Date.now()/1000) + (60 * 60 * 24) }, process.env.JWT_SECRET);
+
+
 
 // Stub Overrides
 
@@ -53,8 +56,133 @@ var appStubs = {
 
 describe('Making the POST to /add', function() {
 
-    it('hits the /add route', function(done) {
-        console.log('test');
-    })
+    var editID = '';
 
-})
+    var snoozeUrl = function() {
+        if (process.env.IP_ADDRESS)
+        {
+            return process.env.IP_ADDRESS + ':80';
+        }
+        else
+        {
+            return 'localhost:8080';
+        }
+    };
+
+    var url = snoozeUrl();
+    var snooze = proxyquire('../index', appStubs);
+
+    describe('app routes - add', function() {
+
+        it('tests if snooze is up', function(done){
+            request(url)
+                .get('/')
+                .expect(200, 'Snooze is up.', done);
+        });
+
+        it('test against /add fails', function(done) {
+            request(url)
+                .post('/add')
+                .set(process.env.JWT_HEADER, token)
+                .send({})
+                .expect(500, 'crap no task specified, or not a valid object wtf?!', done);
+        });
+
+        it('test against /add', function(done) {
+            var date = Date.now();
+            request(url)
+                .post('/add')
+                .set(process.env.JWT_HEADER, token)
+                .send({task:
+                    {
+                        ts: date + 10000,
+                        url: 'http://www.google.com',
+                        status : 1
+                    }
+                })
+                .expect(function(res){
+                    editID = res.body.id;
+                    if(res.statusCode !== 200)
+                    {
+                        throw new Error('status is not 200');
+                    }
+                    if(!res.body.id)
+                    {
+                        throw new Error('incorrect ID being returned');
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                })
+                .end(done);
+        });
+
+        it('only accepts valid json', function(done) {
+            request(url)
+                .post('/add')
+                .set(process.env.JWT_HEADER, token)
+                .send({task : 'String, Not Valid JSON'})
+                .expect(500, 'crap no task specified, or not a valid object wtf?!', done);
+        });
+    });
+
+    describe('app routes - cancel', function() {
+
+        it('cancels a task in the queue', function (done){
+            request(url)
+                .put('/cancel/' + editID)
+                .expect(200)
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(function(res){
+                    if (res.status !== 200)
+                    {
+                        throw new Error('status is not 200');
+                    }
+                    else if (res.body.task.Attributes.status !== 2)
+                    {
+                        throw new Error('incorrect attribute');
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                })
+                .end(done);
+        });
+
+        it('sends back an error if task is not found', function(done) {
+            request(url)
+                .put('/cancel/4')
+                .expect(500)
+                .expect(function(res) {
+                    if (res.body.success === true)
+                    {
+                        throw new Error('Task should not exist');
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                })
+                .end(done);
+        });
+
+    });
+
+    //describe('app routes - Check if event exists', function() {
+    //
+    //    xit('should find an event and return its information', function(done) {
+    //        //is - returns a status saying whether event exists /id/payload - GET
+    //        request(url)
+    //            .get('/' + editID + '/payload')
+    //            .expect(200)
+    //            .expect(function(res) {
+    //
+    //            })
+    //
+    //    });
+    //
+    //});
+
+});
