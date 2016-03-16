586e81d754e8b3a7e0c58ae9c6ceab2512cce2b2
perl5punk <perlpunk@gmail.com>
2016-03-16 14:27:42 -0600

    dump to console if no elk logger host is defined

--- util/logger.js
+++ util/logger.js
@@ -1,11 +1,27 @@
 var elkLogger = require('node-elk-logger');
 
-elkLogger.configure({
-    host: process.env.ELK_LOGGER_HOST,
-    elasticSearchIndexPrefix: 'snooze',
-    messageDecorations: { environment: process.env.ENVIRONMENT },
-    level: process.env.LOGGING_LEVEL,
-    logToConsole: (process.env.ENVIRONMENT != 'Production')
-});
+if (!process.env.ELK_LOGGER_HOST)
+{
+    elkLogger.log = function(message, type, payload){
+        if (type == 'ERROR')
+        {
+            console.error(type+': '+message, payload);
+        }
+        else
+        {
+            console.log(type+': '+message, payload);
+        }
+    };
+}
+else
+{
+    elkLogger.configure({
+        host: process.env.ELK_LOGGER_HOST,
+        elasticSearchIndexPrefix: 'snooze',
+        messageDecorations: { environment: process.env.ENVIRONMENT },
+        level: process.env.LOGGING_LEVEL,
+        logToConsole: (process.env.ENVIRONMENT != 'Production')
+    });
+}
 
 module.exports = elkLogger;
